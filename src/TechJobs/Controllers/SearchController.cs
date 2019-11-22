dc3981b890d5a8dd46676a5e2e2319a63da9252d
Nathalia <natylafosse@gmail.com>
2019-11-21 23:44:21 -0500

    complete

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -14,7 +14,24 @@ namespace TechJobs.Controllers
         }
 
         // TODO #1 - Create a Results action method to process 
-        // search request and display results
+        public IActionResult Results(string searchType, string searchTerm = "")
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            if (searchType.Equals("all"))
+            {
+                //use FindByValue passing in searchTerm and store the values in the list
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                //Use the searchTerm and the searchType and use the FindByColumnAndValue method and save that to the list
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
 
+            return View("Index");
+        }
     }
 }
+
+
